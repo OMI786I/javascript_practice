d9556b8706a008da63a53d9a2ccede623fa39107
OMI786I <sohrawardy1998@gmail.com>
2024-11-25 11:30:57 +0600

    filtering with or without function function from an array

--- withoutJSFunc.js
+++ withoutJSFunc.js
@@ -19,3 +19,26 @@ function double2(arr) {
   let result = arr.map((res) => res * 2);
   return result;
 }
+
+//2) Filter out even numbers
+//without function
+// Input: [1, 2, 3, 4, 5, 6]
+// Output: [2, 4, 6]
+
+function filterEven(arr) {
+  let result = [];
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      // result.push(arr[i]);
+      result[j] = arr[i];
+      j++;
+    }
+  }
+  return result;
+}
+
+function filterEven2(arr) {
+  const result = arr.filter((res) => res % 2 === 0);
+  return result;
+}
